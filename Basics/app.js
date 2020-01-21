556bbbfb7e91aca6303b5c8f46b372d295598a5e
Goplani <aakash.goplani@accenture.com>
2020-01-21 18:35:25 +0530

    2 videos

--- Basics/app.js
+++ Basics/app.js
@@ -11,7 +11,11 @@ http.createServer(function(req, res) {
 }); */
 
 const server = http.createServer((req, res) => {
-    console.log('req :', req);
-    console.log('res :', res);
+    console.log('req url :', req.url);
+    console.log('res headers :', req.headers);
+    console.log('req method :', req.method);
+    /* console.log('res :', res);
+    console.log('req :', req); */
+    process.exit();
 });
 server.listen(4200, 'localhost');

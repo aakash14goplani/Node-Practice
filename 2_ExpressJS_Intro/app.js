d0ac6da6535bae6cfd1c7a6db9e61e3a9e3e9fec
aakash14goplani <goplaniaakash14@gmail.com>
2020-02-16 01:37:29 +0530

    completed EJS

--- 2_ExpressJS_Intro/app.js
+++ 2_ExpressJS_Intro/app.js
@@ -8,20 +8,21 @@ const adminData = require('./routes/admin');
 const shoppingRoutes = require('./routes/shop');
 const rootDirectory = require('./util/path');
 
-const expresHandlebars = require('express-handlebars');
+/* const expresHandlebars = require('express-handlebars');
 
 app.engine('hbs', expresHandlebars({
     defaultLayout: 'main-layout',
     layoutsDir: '2_ExpressJS_Intro/views/layouts/',
     extname: 'hbs'
-}));
+})); */
 /* 
     It's the way you structured your app. If you're starting the server from outside of the folder then you need to provide an absolute path to the views folder.
     COMPARE YOUR FOLDER STRUCTURE WITH MAX...
 */
 app.set('views', path.join(rootDirectory, 'views'));
 // app.set('view engine', 'pug');
-app.set('view engine', 'hbs');
+// app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(rootDirectory, 'public')));
 
